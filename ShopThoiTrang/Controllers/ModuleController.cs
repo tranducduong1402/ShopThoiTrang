262e20284d5dc16a99c90a8f15b1dab77c6ac9ca
DuongTD <tranducduong14022001@gmail.com>
2022-05-20 16:38:52 +0700

    4th commit

---
+++ ShopThoiTrang/Controllers/ModuleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopThoiTrang.Controllers
+{
+    public class ModuleController : Controller
+    {
+        // GET: Module
+        public ActionResult MainMenu()
+        {
+            return View("MainMenu");
+        }
+    }
+}
